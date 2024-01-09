06fa4bc1d6e0e263147df2e144f6e8f27f1b2358
Kyriaki Avgerinou <kyriaki.avgerinou@icloud.com>
2024-01-09 19:02:41 +0200

    feat: Add class Wallet
    This class:
    - Initiates a new Wallet object,
    - generates a public - private key pair for the wallet,
    - signs a given piece of data on behalf of the wallet,
    - verifies the integrity and authenticity of the signature.

---
+++ src/wallet.py
@@ -0,0 +1,45 @@
+import rsa
+
+class Wallet:
+    """
+    The wallet of a node in the network.
+
+    Attributes:
+        public_key (str): the public key of the wallet (also serves as the address of the wallet).
+        private_key (str): the private key of the wallet.
+        balance (int): the total balance of the wallet.
+        transactions (list): the transactions of the wallet.
+    """
+
+    def __init__(self, initial_balance = 0):
+        """Inits a wallet."""
+        self.public_key, self.private_key = self.generate_key_pair()
+        self.balance = initial_balance
+        self.transactions = []
+
+    def __str__(self):
+        """Returns a string representation of the wallet."""
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+# ======================================================================================================================================================
+# Wallet's Cryptographic Operations
+# ======================================================================================================================================================
+
+    def generate_key_pair(self):
+        """Generates the public - private key pair of the wallet using the rsa module."""
+        public_key, private_key = rsa.newkeys(2048) # You can adjust the key size for better key generation timing (i.e. 1024).
+        return public_key.save_pkcs1().decode(), private_key.save_pkcs1().decode()
+
+    def sign_data(self, data):
+        """Returns the digital signature for the given piece of data using the private key of the wallet."""
+        signature = rsa.sign(data.encode(), rsa.PrivateKey.load_pkcs1(self.private_key), "SHA-256")
+        return signature
+
+    def verify_signature(self, data, signature, public_key):
+        """Verifies whether the signature on the given piece of data derives from the wallet with the given public key (True) or not (False).
+            It also detects whether the given data has been altered or tampered with."""
+        try:
+            rsa.verify(data.encode(), signature, rsa.PublicKey.load_pkcs1(public_key))
+            return True
+        except rsa.VerificationError:
+            return False
