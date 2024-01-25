de6a363ef2fa9cfe69f4f59367f94043099eeb72
Kyriaki Avgerinou <kyriaki.avgerinou@icloud.com>
2024-01-25 21:59:28 +0200

    feat: Add global variable total_nodes
    For the total number of nodes in the network.

--- src/config.py
+++ src/config.py
@@ -1,7 +1,14 @@
 block_capacity = 5 # default value for the capacity of the blocks
+total_nodes = 0 # initialize the total number of nodes in the network
 
 def set_block_capacity(value):
     """Updates the global variable block_capacity with the given value."""
     # The given value should be passed to the main program as a command-line argument.
     global block_capacity
     block_capacity = value
+
+def set_total_nodes(value):
+    """Updates the global variable total_nodes with the given value."""
+    # The given value should be passed to the main program as a command-line argument.
+    global total_nodes
+    total_nodes = value
