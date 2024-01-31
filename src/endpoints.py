c6f9768f72830da711dcb4967a17e7f2e0bbfa09
Kyriaki Avgerinou <kyriaki.avgerinou@icloud.com>
2024-01-31 22:13:19 +0200

    feat: Add Endpoint - Register new node
    When a new node, other than the bootstrap, joins the network,
    they send an HTTP POST request to the bootstrap node.
    The bootstrap node registers the new node
    and integrates them into the network ring.

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from node import Node
 
 # Init the current node.
@@ -7,8 +7,22 @@ node = Node()
 # Define the blockchat_bp blueprint for the endpoints.
 blockchat_bp = Blueprint("blockchat_bp", __name__)
 
-"""
-@blockchat_bp.route("/request", methods = [""])
-def request():
-    pass
-"""
+@blockchat_bp.route("/register_node", methods = ["POST"])
+def register_node():
+    """
+    Request to the bootstrap node only:
+        - Register a new node in the network and add it to the ring.
+    """
+    # Calculate the id for the new node.
+    new_node_id = len(node.ring)
+
+    # Retrieve the new node data from the request.
+    new_node_ip = request.form.get("ip")
+    new_node_port = request.form.get("port")
+    new_node_public_key = request.form.get("public_key")
+
+    # Register the new node.
+    node.register_node_to_ring(new_node_id, new_node_ip, new_node_port, new_node_public_key)
+
+    # Return the id of the new node.
+    return jsonify({"id": new_node_id})
