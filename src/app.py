dc5028fd34d5b7491fedb070f22edf82698c22f4
Kyriaki Avgerinou <kyriaki.avgerinou@icloud.com>
2024-02-07 14:13:26 +0200

    feat: Welcome a new node in the network
    A new node other than the bootstrap node.
    
    When a new node enters the network, ask the bootstrap:
    - to register them,
    - add them to the ring of nodes,
    - and give them their id.
    
    If the new node is the last one to enter the network:
    - send bootstrap's final ring of nodes to all the nodes in the network,
    - send bootstrap's initial version of the blockchain,
    with only the genesis block, to all the nodes in the network.

--- src/app.py
+++ src/app.py
@@ -3,6 +3,10 @@ from endpoints import node, blockchat_bp
 from argparse import ArgumentParser
 import config
 from blockchain import Blockchain
+import socket
+import pickle
+import threading
+from libraries.module_library import make_get_request, make_post_request
 
 # Get the ip address and the port of the bootstrap node defined by the config file.
 BOOTSTRAP_IP = config.BOOTSTRAP_IP
@@ -52,4 +56,59 @@ if __name__ == "__main__":
         # app.run(debug = True, host = node.ip, port = node.port)
         app.run(host = node.ip, port = node.port) # run the Flask development server and specify the ip address and port on which the Flask server should listen
     else:
-        pass
+        """
+        If the current node is not the bootstrap node:
+            - Initiate the current node,
+            - ask the bootstrap node to register them, add them to the ring of nodes and give them their id.
+        If the current node is the last one to enter the network:
+            - send bootstrap's FINAL ring of nodes to all the nodes in the network,
+            - send bootstrap's initial version of the blockchain to all the nodes in the network.
+        """
+        # Get the ip address of the device.
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+
+        # Initialize the current node.
+        node.ip = ip_address
+        node.port = port
+
+        # Send an HTTP POST request to the bootstrap node to register the current node.
+        node_data = {
+            "ip": node.ip,
+            "port": node.port,
+            "public_key": node.wallet.public_key
+        }
+        registration_response = make_post_request(BOOTSTRAP_IP, BOOTSTRAP_PORT, endpoint = "register_node", data = node_data)
+
+        # Retrieve the id for the current node.
+        node.id = registration_response.json()["id"]
+
+        # If the current node is the last one to enter the network,
+        # send the final ring of nodes and the initial blockchain with only the genesis block
+        # to all the nodes in the network.
+        if node.id == total_nodes - 1:
+
+            # Get the final ring of nodes from the bootstrap node.
+            bootstrap_ring = make_get_request(BOOTSTRAP_IP, BOOTSTRAP_PORT, endpoint = "get_ring")
+            bootstrap_ring = bootstrap_ring.json()["ring"]
+
+            # Send the final ring and the initial blockchain to all the nodes in the network.
+            def thread_function(): # there were concurrency issues that were resolved via threading
+                # When you start a new thread to handle the loop,
+                # it runs concurrently with the main thread,
+                # which continues executing the subsequent code,
+                # including starting the Flask server for the last node.
+                # By the time the POST requests refer to the last node,
+                # its server will be up.
+                for ring_node in bootstrap_ring[1:]: # exclude the bootstrap node
+                    # Post the bootstrap_ring to ring_node's ring.
+                    post_ring_response = make_post_request(ring_node["ip"], ring_node["port"], endpoint = "post_ring", data = pickle.dumps(bootstrap_ring))
+                    # Ask the bootstrap node to post its self.chain to ring_node's chain.
+                    post_chain_response = make_post_request(BOOTSTRAP_IP, BOOTSTRAP_PORT, endpoint = "ask_chain", data = pickle.dumps(ring_node))
+                    print(post_ring_response.json()["message"])
+                    print(post_chain_response.json()["message"])
+            req = threading.Thread(target = thread_function, args = ())
+            req.start()
+
+        # app.run(debug = True, host = node.ip, port = node.port)
+        app.run(host = node.ip, port = node.port) # run the Flask development server and specify the ip address and port on which the Flask server should listen
